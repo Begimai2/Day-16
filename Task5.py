429727d9d6aec84a10019f8326b3e055a40ead75
Begimai2 <begi.ab@mail.ru>
2020-07-17 14:30:52 +0600

    Day-16

---
+++ Task5.py
@@ -0,0 +1,6 @@
+guests = ['Jihyo', 'Momo', 'Dahyun', 'Sana', 'Mina']
+guests.insert(0, 'Nayeon')
+guests.insert(3, 'Jeongyeon')
+guests.append('Chaeyoung')
+print(guests)
+print('We have more guests')
